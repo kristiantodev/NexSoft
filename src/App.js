0f6c24382fd0ddcc91d74bd3f5ef95afbc103c01
Kristianto <kristiantorpl@gmail.com>
2021-02-11 15:39:25 +0700

    Ujian React Aplikasi Penjagalan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Header from "./template/header"
+// import Nav from "./template/navigation"
+// import Footer from "./template/footer"
+
+import { Header, Nav, Body, Footer } from "./template"
+import "./App.css"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: "home"
+    }
+  }
+
+  goToPage = page => {
+    this.setState({
+      currentPage: page
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Nav page={this.state.currentPage} changePage={this.goToPage} />
+        <Body page={this.state.currentPage} />
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
